0a364bae02d70d4c598718e613c28174f27a14f5
Jeffry James <jeffryjamesh@gmail.com>
2021-09-21 21:21:11 +0530

    Add files via upload

---
+++ face detection dataset.py
@@ -0,0 +1,33 @@
+import cv2, os
+haar_file = 'D:/Python/DL/Day 5/haarcascade_frontalface_default.xml'
+datasets = 'dataset'  
+name = 'jeff'     
+
+path = os.path.join(datasets, name)
+if not os.path.isdir(path):
+    os.makedirs(path)
+
+(width, height) = (130, 100)
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0)
+
+count = 1
+while count < 31:
+    print(count)
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite('%s/%s.png' % (path,count), face_resize)
+    count += 1
+	
+    cv2.imshow('OpenCV', im)
+    key = cv2.waitKey(10)
+    if key == ord('q'):
+        break
+print("Dataset obtained successfully")
+webcam.release()
+cv2.destroyAllWindows()
